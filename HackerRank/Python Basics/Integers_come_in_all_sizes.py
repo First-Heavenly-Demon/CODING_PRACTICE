062bb29575de644bf90f7b8dddeea2c4ecd66540
First-Heavenly-Demon <borobiswajit123vol2@gmail.com>
2025-08-25 18:22:36 +0530

    Create Integers_come_in_all_sizes.py
    It was easy, I forgot to see the + operator and made mistake.

---
+++ HackerRank/Python Basics/Integers_come_in_all_sizes.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+First_number = int(input())
+Second_number = int(input())
+Third_number = int(input())
+Fourth_number = int(input())
+First_square = First_number ** Second_number + Third_number ** Fourth_number
+print(First_square, end = "")
