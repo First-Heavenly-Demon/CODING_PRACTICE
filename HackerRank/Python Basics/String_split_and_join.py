6153377a49ccfb94afa24609f11b994525872312
First-Heavenly-Demon <borobiswajit123vol2@gmail.com>
2025-09-05 19:27:53 +0530

    Update String_split_and_join.py
    i got to know how to split a line into lists and then use a "-" to join it.

--- HackerRank/Python Basics/String_split_and_join.py
+++ HackerRank/Python Basics/String_split_and_join.py
@@ -1,1 +1,9 @@
-trying something new
+# trying something new
+def split_and_join(line):
+    line_parts = line.split()
+    return "-".join(line_parts)
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
